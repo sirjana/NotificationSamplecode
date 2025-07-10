3a2209c655693452f89c65387627786d526d6d7a
sirjana <sinapandey4@gmail.com>
2025-07-10 15:44:36 +0545

    Update Smsnotification.java

--- notification/Smsnotification.java
+++ notification/Smsnotification.java
@@ -4,7 +4,7 @@ public class Smsnotification implements Inotification {
     @Override
     public void sendNotification(String message) {
         System.out.println("Sending SMS notification" +message);
-        System.out.println("Sending SMS notification with ci/cd" +message);
+        System.out.println("Sending SMS notification1" +message);
     }
 
     
